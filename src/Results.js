da366443679f13aa302269ec23a4698d6ab68ab0
Malaysia Thomas <105211177+MalaysiaThomas@users.noreply.github.com>
2025-08-04 11:22:23 -0400

    Update results display

--- src/Results.js
+++ src/Results.js
@@ -2,26 +2,28 @@ import React from "react";
 import "./Results.css";
 
 export default function Results(props) {
-    console.log("RESULTS");
-    console.log(props.results);
-    // let word = props.results.word;
-    // let phonetic = props.results.phonetic;
-    // let partOfSpeech = props.results.meanings[0].partOfSpeech;
-    // let definition = props.results.meanings[0].definition
     
+    let word = props.results.word;
+    let phonetic = props.results.phonetic;
+    let partOfSpeech = props.results.meanings[0].partOfSpeech;
+    let definition = props.results.meanings[0].definition
+    let synonyms = props.results.meanings[0].synonyms;
+
 
     if (props.results) {
         return (
           <div className="Results">
-            {/* <h3>{word}</h3>
+            <h3>{word}</h3>
             <h4>{phonetic}</h4>
-            <p>{partOfSpeech}</p> */}
+            <p>{partOfSpeech}</p>
+            <p>{definition}</p>
+            <h5>Synonyms:</h5>
 
-            {/* <ul>
-              <li>{props.results.meaning[0].synonyms[0]}</li>
-              <li>{props.results.meaning[0].synonyms[1]}</li>
-              <li>{props.results.meaning[0].synonyms[2]}</li>
-            </ul> */}
+            <ul>
+                <li>{synonyms[0]}</li>
+                <li>{synonyms[1]}</li>
+                <li>{synonyms[2]}</li>
+            </ul>
           </div>
         );
     } else {
