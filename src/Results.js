b90fad7601c612aed0c1e2e96dae5e6637606b24
Malaysia Thomas <105211177+MalaysiaThomas@users.noreply.github.com>
2025-08-04 11:55:40 -0400

    fix result function

--- src/Results.js
+++ src/Results.js
@@ -2,30 +2,28 @@ import React from "react";
 import "./Results.css";
 
 export default function Results(props) {
-    
-    let word = props.results.word;
-    let phonetic = props.results.phonetic;
-    let partOfSpeech = props.results.meanings[0].partOfSpeech;
-    let definition = props.results.meanings[0].definition
-    let synonyms = props.results.meanings[0].synonyms;
-
-
+    console.log(props.results);
     if (props.results) {
+        let word = props.results.word;
+        let phonetic = props.results.phonetic;
+        let partOfSpeech = props.results.meanings[0].partOfSpeech;
+        let definition = props.results.meanings[0].definition;
+        let synonyms = props.results.meanings[0].synonyms;
         return (
-          <div className="Results">
-            <h3>{word}</h3>
-            <h4>{phonetic}</h4>
-            <p>{partOfSpeech}</p>
-            <p>{definition}</p>
-            <h5>Synonyms:</h5>
+            <div className="Results">
+                <h3>{word}</h3>
+                <h4>{phonetic}</h4>
+                <p>{partOfSpeech}</p>
+                <p>{definition}</p>
+                <h5>Synonyms:</h5>
 
-            <ul>
+                <ul>
                 <li>{synonyms[0]}</li>
                 <li>{synonyms[1]}</li>
                 <li>{synonyms[2]}</li>
-            </ul>
-          </div>
-        );
+                </ul>
+            </div>
+            );
     } else {
         return null;
     }
