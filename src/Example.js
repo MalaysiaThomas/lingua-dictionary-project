00856cb73fdc2e4a9b28fb99536c3bfb94b7b113
Malaysia Thomas <105211177+MalaysiaThomas@users.noreply.github.com>
2025-08-06 13:03:20 -0400

    Create Example.js

---
+++ src/Example.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Example(props) {
+    if (props.example) {
+        return (
+            <div className="Example">
+                <p>Example:<em> "{props.example}"</em></p>
+            </div>
+        );
+    } else {
+        return null;
+    }
+}
