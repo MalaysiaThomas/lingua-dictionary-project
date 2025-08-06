30f0cbf770f43afef1514ae79a77cccf8f5303fe
Malaysia Thomas <105211177+MalaysiaThomas@users.noreply.github.com>
2025-08-06 12:51:35 -0400

    Create Synonyms.js

---
+++ src/Synonyms.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Synonyms(props) {
+    if (props.synonyms && props.synonyms.length > 0) {
+        return (
+            <div className="Synonyms">
+                <h5>Synonyms:</h5>
+                <ul>
+                    {props.synonyms.map((synonym, index) => (
+                        <li key={index}>{synonym}</li>
+                    ))}
+                </ul>
+            </div>
+        );
+    } else {
+        return null;
+    }
+}
